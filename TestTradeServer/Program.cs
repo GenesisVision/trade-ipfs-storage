1cb0e784ed1ede1c693ada9609125e459f497b84
Dmitriy Ugarov <d.ugarov@genesis.vision>
2017-12-08 16:45:24 +0300

    Test trade server

--- TestTradeServer/Program.cs
+++ TestTradeServer/Program.cs
@@ -1,5 +1,6 @@
 ﻿using GenesisVision.TradeIpfsStorage;
 using System;
+using System.Threading;
 
 namespace TestTradeServer
 {
@@ -10,6 +11,22 @@ namespace TestTradeServer
             try
             {
                 var ipfsStorage = new TradeIpfsStorage("http://localhost:5001");
+
+                var testServer = new TestMetaTrader4Server();
+                testServer.Start();
+
+                while (true)
+                {
+                    var tradeContainer = testServer.GeTradeContainer();
+                    var hash = ipfsStorage.UploadTrades(tradeContainer);
+
+                    if (!string.IsNullOrEmpty(hash))
+                    {
+                        var data = ipfsStorage.GetTrades(hash);
+                    }
+
+                    Thread.Sleep(5 * 1000);
+                }
             }
             catch (Exception e)
             {
---
+++ TestTradeServer/TestMetaTrader4Server.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Timers;
+using GenesisVision.TradeIpfsStorage.Interfaces;
+using GenesisVision.TradeIpfsStorage.Models;
+
+namespace TestTradeServer
+{
+    public class TestMetaTrader4Server
+    {
+        private readonly TradeContainer tradeContainer;
+        private readonly List<IBaseTrade> trades;
+        private readonly Timer timer;
+        private readonly Random random;
+        private readonly object lockObj;
+
+        public TestMetaTrader4Server()
+        {
+            tradeContainer = new TradeContainer(new Server("127.0.0.1", "Test Server", ServerType.MetaTrader4), 1);
+            trades = new List<IBaseTrade>();
+            lockObj = new object();
+            random = new Random();
+            timer = new Timer(random.Next(1, 6) * 100);
+            timer.Elapsed += (sender, args) => CreateTrade();
+        }
+
+        public void Start()
+        {
+            timer.Start();
+        }
+
+        public void Stop()
+        {
+            timer.Stop();
+        }
+
+        private void CreateTrade()
+        {
+            var trade = new Trade
+            {
+                TicketNum = random.Next(100000, 500000),
+                Ticket = "",
+                LoginNum = random.Next(500000, 900000),
+                Login = "",
+                DateOpen = DateTime.Now.AddMinutes(-random.Next(200, 500)),
+                DateClose = DateTime.Now.AddMinutes(random.Next(200, 500)),
+                Direction = Direction.Buy,
+                PriceOpen = 1 - (random.Next(0, 100) * 0.001m),
+                PriceClose = 1 + (random.Next(0, 100) * 0.001m),
+                Profit = random.Next(100, 500),
+                Symbol = "TEST",
+                Volume = random.Next(1, 10)
+            };
+            lock (lockObj)
+            {
+                trades.Add(trade);
+            }
+        }
+
+        public TradeContainer GeTradeContainer()
+        {
+            lock (lockObj)
+            {
+                tradeContainer.Trades = trades.ToList();
+                trades.Clear();
+                timer.Interval = random.Next(1, 6) * 100;
+
+                return tradeContainer;
+            }
+        }
+    }
+}
